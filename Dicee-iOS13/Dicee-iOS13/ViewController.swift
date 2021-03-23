1873dc4e9750edf7bf99f84531d98097b44bda1f
Amina Kobenova <56433807+ak7588@users.noreply.github.com>
2021-03-23 14:35:24 +0600

    Add files via upload

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -13,20 +13,14 @@ class ViewController: UIViewController {
     // do this by pressing ctrl + drag to here
     @IBOutlet weak var diceImageViewTwo: UIImageView!
     @IBOutlet weak var diceImageViewOne: UIImageView!
-    
-    // do this when opening the app
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // who.what = behavior
-        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
-    }
+
     
     // do this when btn is pressed
     @IBAction func buttonPressed(_ sender: UIButton) {
-        diceImageViewOne.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceFour")
-        print("Button Pressed")
+        // Int.random(in: 0...5)
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+        diceImageViewOne.image = diceArray.randomElement()
+        diceImageViewTwo.image = diceArray.randomElement()
     }
     
 }
