d270d9d019b38f6f22c746dbb99569b21bda6008
Amina Kobenova <56433807+ak7588@users.noreply.github.com>
2021-05-26 17:56:16 +0600

    Add files via upload

--- Calculator-Layout-iOS13/Calculator Layout iOS13/ViewController.swift
+++ Calculator-Layout-iOS13/Calculator Layout iOS13/ViewController.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var isFinishedTyping = true
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        isFinishedTyping = true
+        guard let number = Double(textLabel.text!)! else {
+            fatalError()
+        }
+        
+        var operation = sender.currentTitle!
+        if operation == "+" {
+            
+        }
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        if let numValue = sender.currentTitle {
+            if isFinishedTyping {
+                textLabel.text = numValue
+                isFinishedTyping = false
+            } else {
+                textLabel.text! += numValue
+            }
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
