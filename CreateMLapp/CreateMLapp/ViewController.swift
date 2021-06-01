e43449c4c53ef57d98bf92808222b45a4f50afbb
Amina Kobenova <56433807+ak7588@users.noreply.github.com>
2021-06-01 15:06:46 +0600

    Add files via upload

---
+++ CreateMLapp/CreateMLapp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CreateMLapp
+//
+//  Created by Amina on 6/1/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+}
+
